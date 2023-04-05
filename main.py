92893869b52cd9ab8fd775bfaaec7aa141aba108
Axel <axel75puech@gmail.com>
2023-04-05 17:31:13 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# from textwrapper import TextWrapper
+
+
+import openai
+
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        return infile.read()
+    
+openai.api_key = open_file('api_key.txt')
+
+completion = openai.ChatCompletion.create(
+  model="gpt-4",
+  messages=[{"role": "system", "content": "the answer must be less than 50 words "},
+      {"role": "user", "content": "Tell me how to cook pasta "}]
+)
+
+print(completion)
